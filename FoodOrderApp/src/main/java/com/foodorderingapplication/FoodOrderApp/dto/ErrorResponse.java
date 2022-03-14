44d739e43476f01d41c1119aafa0e35832f84796
DiegoTruP <63624137+DiegoTruP@users.noreply.github.com>
2022-03-13 22:30:17 -0600

    Order detail added

--- FoodOrderApp/src/main/java/com/foodorderingapplication/FoodOrderApp/dto/ErrorResponse.java
+++ FoodOrderApp/src/main/java/com/foodorderingapplication/FoodOrderApp/dto/ErrorResponse.java
@@ -1,10 +1,21 @@
 package com.foodorderingapplication.FoodOrderApp.dto;
 
+import java.time.LocalDateTime;
+
 public class ErrorResponse {
 	
 	private String message;
 	private String statusCode;
+	private LocalDateTime date;
 	
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
 	public ErrorResponse(String message, String statusCode) {
 		super();
 		this.message = message;
