487ba0214d88a6819fc625b2cfe48d527b79a13e
DiegoTruP <63624137+DiegoTruP@users.noreply.github.com>
2022-03-16 09:39:14 -0600

    Order Detail View deleted

--- FoodOrderApp/src/main/java/com/foodorderingapplication/FoodOrderApp/repo/OrderDetailRepo.java
+++ FoodOrderApp/src/main/java/com/foodorderingapplication/FoodOrderApp/repo/OrderDetailRepo.java
@@ -6,19 +6,12 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import com.foodorderingapplication.FoodOrderApp.dto.OrderDetailDTO;
-import com.foodorderingapplication.FoodOrderApp.dto.OrderDetailView;
 import com.foodorderingapplication.FoodOrderApp.entity.OrderDetail;
-import com.foodorderingapplication.FoodOrderApp.entity.OrderProduct;
+
 
 public interface OrderDetailRepo extends JpaRepository<OrderDetail, Integer>{
 
 	List<OrderDetail> findAllByUserId(Integer userId, Pageable pages);
-	
-
-	List<OrderDetailView> findAllDTOByUserId(Integer userId);
 	
 
 }
