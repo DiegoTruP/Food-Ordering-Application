eda045666bac8f6c3653710226ea382ff5d325e0
DiegoTruP <63624137+DiegoTruP@users.noreply.github.com>
2022-03-18 07:15:54 -0600

    User controller and serv Test added

--- FoodOrderApp/src/main/java/com/foodorderingapplication/FoodOrderApp/dto/UserRequestDTO.java
+++ FoodOrderApp/src/main/java/com/foodorderingapplication/FoodOrderApp/dto/UserRequestDTO.java
@@ -1,6 +1,8 @@
 package com.foodorderingapplication.FoodOrderApp.dto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserRequestDTO {
@@ -10,6 +12,10 @@ public class UserRequestDTO {
 	@NotEmpty(message = "Password should not be emty")
 	@Size(min=8,message="Password must have more than 7 characters")
 	private String password;
+	@NotNull
+	@NotEmpty
+	@Min(1)
+	private Integer userId;
 	
 	public String getUserName() {
 		return userName;
@@ -23,4 +29,12 @@ public class UserRequestDTO {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	
 }
---
+++ FoodOrderApp/src/test/java/com/foodorderingapplication/FoodOrderApp/controller/UserControllerTest.java
@@ -0,0 +1,84 @@
+package com.foodorderingapplication.FoodOrderApp.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.foodorderingapplication.FoodOrderApp.dto.UserRequestDTO;
+import com.foodorderingapplication.FoodOrderApp.repo.UserRepo;
+import com.foodorderingapplication.FoodOrderApp.service.UserService;
+import com.foodorderingapplication.FoodOrderApp.dto.UserResponseDTO;
+
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+	
+	@MockBean
+	UserService userService;
+	
+	@Mock
+	UserRepo userRepo;
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	UserRequestDTO userRequestDTO;
+	UserResponseDTO userResponseDTO;
+	
+	@BeforeEach
+	public void setUp() {
+		userRequestDTO = new UserRequestDTO();
+		userRequestDTO.setUserId(1);
+		userRequestDTO.setUserName("Kevin");
+		userRequestDTO.setPassword("123456780");
+		
+		userResponseDTO = new UserResponseDTO("Login Successful",200);
+		userResponseDTO.setUserId(1);
+		
+	}
+	
+	@Test
+	public void LoginTest() throws Exception {
+		
+		
+		when(userService.AuthenticateUser(any(UserRequestDTO.class))).thenReturn(userResponseDTO);
+		
+		MvcResult result = mockMvc.perform(post("/Login").content(asJsonString(userRequestDTO)).contentType(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())		
+			.andExpect(jsonPath("$.statusCode").value(200))
+			.andExpect(jsonPath("$.userId").value(1)).andReturn();
+		
+		//mockMvc.perform(asyncDispatch(result));
+		String responseBody = result.getResponse().getContentAsString();
+		
+		System.out.println(responseBody);
+		
+	}
+
+	private String asJsonString(UserRequestDTO userRequestDTO2) {
+
+		try {
+			return new ObjectMapper().writeValueAsString(userRequestDTO2);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
