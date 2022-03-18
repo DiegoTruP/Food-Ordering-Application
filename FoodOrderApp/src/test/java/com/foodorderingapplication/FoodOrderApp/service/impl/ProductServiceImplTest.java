0b8a795c1a77aef295441abea1733b4e32c1213d
Alejandra Escobar <ale.fesico@gmail.com>
2022-03-18 03:35:56 -0600

    Product Service integration tests implementation

--- FoodOrderApp/src/test/java/com/foodorderingapplication/FoodOrderApp/service/impl/ProductServiceImplTest.java
+++ FoodOrderApp/src/test/java/com/foodorderingapplication/FoodOrderApp/service/impl/ProductServiceImplTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,7 +18,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.foodorderingapplication.FoodOrderApp.dto.ProductDetails;
 import com.foodorderingapplication.FoodOrderApp.dto.ProductRequestDTO;
+import com.foodorderingapplication.FoodOrderApp.dto.ProductResponseDTO;
 import com.foodorderingapplication.FoodOrderApp.entity.Address;
 import com.foodorderingapplication.FoodOrderApp.entity.Product;
 import com.foodorderingapplication.FoodOrderApp.entity.ProductCategory;
@@ -40,6 +43,9 @@ public class ProductServiceImplTest {
 	
 	ProductRequestDTO productRequestDto;
 	Store store;
+	ProductDetails productDetails1;
+	ProductDetails productDetails2;
+	ProductDetails productDetails3;
 	
 	@BeforeEach
 	public void init() {
@@ -73,6 +79,29 @@ public class ProductServiceImplTest {
 		store.setRating(4);
 		store.setStoreAddress(address);
 		
+		productDetails1 = new ProductDetails();
+		productDetails1.setProductId(3);
+		productDetails1.setProductName("Jabon vanish");
+		productDetails1.setProductDescription("Jabon liquido para ropa color");
+		productDetails1.setProductPrice(450);
+		productDetails1.setProductCategory(ProductCategory.CENAS);
+		productDetails1.setAvailable(true);
+		
+		productDetails2 = new ProductDetails();
+		productDetails2.setProductId(5);
+		productDetails2.setProductName("Te Canela");
+		productDetails2.setProductDescription("Te de canela con rallado de jengibre");
+		productDetails2.setProductPrice(99);
+		productDetails2.setProductCategory(ProductCategory.BEBIDAS);
+		productDetails2.setAvailable(false);
+		
+		productDetails3 = new ProductDetails();
+		productDetails3.setProductId(10);
+		productDetails3.setProductName("Ensalada con semillas");
+		productDetails3.setProductDescription("Ensalada de lechuga con manzanas y grasas saludables");
+		productDetails3.setProductPrice(320);
+		productDetails3.setProductCategory(ProductCategory.VEG);
+		productDetails3.setAvailable(true);
 	}
 	
 	@Test
@@ -102,5 +131,32 @@ public class ProductServiceImplTest {
 		when(storeRepo.findById(2)).thenReturn(Optional.empty());
 		
 		assertThrows(StoreNotFoundException.class, () -> productServiceImpl.saveProductDetails(productRequestDto));
+	}
+	
+	@Test
+	@DisplayName("Get All Products By Store: POSITIVE")
+	public void getAllProductsByStoreTest() {
+		// stub storeRepo.findById
+		when(storeRepo.findById(2)).thenReturn(Optional.of(store));
+		
+		// stub productRepo.findAllProductsByStore
+		when(productRepo.findAllProductsByStore(any(Store.class))).thenReturn(List.of(productDetails1, productDetails2, productDetails3));
+		
+		ProductResponseDTO productResponseDto = productServiceImpl.getAllProductsByStore(2);
+		assertNotNull(productResponseDto);
+		assertEquals(10, productResponseDto.getProductList().get(2).getProductId());
+		assertEquals(3, productResponseDto.getProductList().size());
+		assertEquals(ProductCategory.BEBIDAS, productResponseDto.getProductList().get(1).getProductCategory());
+		assertEquals(200, productResponseDto.getStatusCode());
+	}
+	
+	@Test
+	@Disabled
+	@DisplayName("Get All Products By Store: NEGATIVE")
+	public void getAllProductsByStoreTestNegative() {
+		// stub storeRepo.findById
+		when(storeRepo.findById(5)).thenReturn(Optional.empty());
+		
+		assertThrows(StoreNotFoundException.class, () -> productServiceImpl.getAllProductsByStore(5));
 	}
 }
